f85669fe547b5629bc7f8d0278c8b77afb0692ab
Marat <maratsargsyan48@gmail.com>
2024-09-15 12:59:44 +0400

    last version

--- demo/demo.kt
+++ demo/demo.kt
@@ -1,3 +1,5 @@
+import java.lang.IllegalArgumentException
+
 Language Paradigms
 1. imperative -how
     1.1 procedural
@@ -16,6 +18,21 @@ Loopsely Coupled VS Tightly Coupled
 
 PROCESSES AND THREADS
 
+OOP
+    1. Encapsulation
+    2. Inheritance
+    3. Polymorphism
+    4. Abstraction
+
+
+
+open class Animal {
+    fun eat() {
+        println("Animal is eating")
+    }
+}
+
+class Tiger
 
 private  var userState = "ready"
 
@@ -32,4 +49,30 @@ fun main(args: Array<String>) {
     println(userState)
 
     println(listOf(1, 2, 3, 4, 5, 6).filter { i -> i % 2 == 0 })
+
+    val user = User()
+    user.id = 1
+    user.name = "Meri"
+}
+
+fun sum(number1: Int, number2: Int) = number1 + number2
+
+class User {
+    var id: Int = 0
+        set(value) {
+            if (id > 0) {
+                field = value
+            } else{
+                throw IllegalArgumentException("Wrong id $id. Id must be a positive")
+            }
+        }
+    private var name: String = ""
+
+    fun setId(id: Int) {
+        this.id = id
+    }
+
+    fun getId() = id
+
+    fun set Name(name: String = "")
 }
